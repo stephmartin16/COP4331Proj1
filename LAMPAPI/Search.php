e696602a0c69f50067d4c54f7521d0d3df4482fc
rundex0 <69157728+rundex0@users.noreply.github.com>
2023-05-29 18:57:47 -0400

    Add files via upload
    Searches Contacts. Returns Contacts as JSON objects

---
+++ LAMPAPI/Search.php
@@ -0,0 +1,74 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID=?");
+		$Name = "%" . $inData["search"] . "%";
+		$stmt->bind_param("sss", $Name, $Name, $inData["UserID"]);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+//			$searchResults .= '"' . $row["FirstName"] . '"';
+            $searchResults .= '{"FirstName" : "' . $row["FirstName"]. '",
+                                "LastName" : "' . $row["LastName"]. '",
+                                "Phone" : "' . $row["Phone"]. '",
+                                "Email" : "' . $row["Email"]. '",
+                                "UserID" : "' . $row["UserID"].'",
+                                "ID" : "' . $row["ID"]. '"}';
+		}
+
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Recordszzx Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
